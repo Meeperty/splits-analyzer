234704f5f45e07bb90c80b3d728c6485a390f1ae
Meeperty <meepprograms@gmail.com>
2022-08-31 12:52:52 -0700

    get segment history from file

--- Models/Splits.cs
+++ Models/Splits.cs
@@ -14,14 +14,18 @@ namespace SplitsAnalyzer.Models
 
         public int attemptCount;
 
+        public List<Segment> segmentList;
+
         internal Splits(string path)
         {
             //in case the file is invalid
             gameName = "Not Found";
             categoryName = "Not Found";
             attemptCount = -1;
+            segmentList = new();
             try
             {
+#pragma warning disable CS8600, CS8602
                 //load and analyze document if possible
                 XmlDocument document = new();
                 document.Load(path);
@@ -41,20 +45,84 @@ namespace SplitsAnalyzer.Models
                 XmlNode attemptCountNode = document.SelectSingleNode("//AttemptCount");
                 if (attemptCountNode != null)
                 {
-                    int.TryParse(attemptCountNode.InnerText, out attemptCount);
+                    attemptCount = int.Parse(attemptCountNode.InnerText);
                 }
+
+                XmlNodeList segmentNodeList = document.SelectNodes("//Segment");
+                if (segmentNodeList != null)
+                {
+                    for (int i = 0; i < segmentNodeList.Count; i++)
+                    {
+                        XmlNode currentSegmentNode = segmentNodeList.Item(i);
+                        
+                        //get segment name
+                        Segment currentSegment = new("Error, Name wasn't found");
+                        string name = currentSegmentNode["Name"].InnerText;
+                        if (name != null)
+                        {
+                            currentSegment = new("Name");
+                        }
+
+                        //get segment history
+                        currentSegment.segmentHistory = new();
+                        XmlNode segmentHistoryNode = currentSegmentNode["SegmentHistory"];
+                        XmlNodeList segmentTimesNodes = segmentHistoryNode.SelectNodes("Time");
+                        if (segmentTimesNodes.Count > 0)
+                        {
+                            for (int j = 0; j < segmentTimesNodes.Count; j++)
+                            {
+                                XmlNode currentTimeNode = segmentTimesNodes[j];
+                                
+                                int attemptId = int.Parse(currentTimeNode.Attributes["id"].Value);
+
+                                TimeSpan gameTime = new();
+                                TimeSpan realTime = new();
+
+                                XmlNode? gameTimeNode = currentTimeNode["GameTime"];
+                                XmlNode? realTimeNode = currentTimeNode["RealTime"];
+
+                                if (gameTimeNode != null)
+                                {
+                                    gameTime = TimeSpan.Parse(gameTimeNode.InnerText);
+                                    currentSegment.segmentHistory.Add(attemptId, gameTime);
+                                }
+                                else
+                                {
+                                    if (realTimeNode != null)
+                                    {
+                                        realTime = TimeSpan.Parse(realTimeNode.InnerText);
+                                        currentSegment.segmentHistory.Add(attemptId, realTime);
+                                    }
+                                    else
+                                    {
+                                        
+                                    }
+                                }
+                            }
+                        }
+
+                        segmentList.Add(currentSegment);
+                    }
+                }
+#pragma warning restore CS8600, CS8602
             }
-            catch
+            catch (NullReferenceException)
             {
                 
             }
         }
     }
-
-    internal class Split
+    
+    internal class Segment
     {
-        string name;
+        public string name;
+        public Dictionary<int, TimeSpan> segmentHistory;
         
+        internal Segment(string name)
+        {
+            this.name = name;
+            segmentHistory = new();
+        }
     }
     
     internal class Attempt
